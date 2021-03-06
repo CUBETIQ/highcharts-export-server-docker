c7aa6016ce3d6a7f306a0227b692cf2b5d86d02d
Sambo Chea <sombochea@cubetiqs.com>
2021-03-06 21:35:25 +0700

    Updated readme and change run mode

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,8 @@ REGISTRY=registry.kh.cubetiqs.com
 IMAGE=highcharts-export-server
 CONTAINER=${REGISTRY}/${IMAGE}:${TAG}
 
+echo "Building container image with ${CONTAINER}"
 docker build -t ${CONTAINER} .
 
+echo "Publishing the container image to ${REGISTRY}"
 docker push ${CONTAINER}
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "Stop and remove container..."
+docker rm -f highcharts-export-server
+
+echo "Run the container..."
 docker run -d --name highcharts-export-server -p 8080:8080 registry.kh.cubetiqs.com/highcharts-export-server
